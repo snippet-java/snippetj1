4ae834fa0bd84818348ac53e443dc1a48923cca1
Cheok Swin Voon <cheoksv@my.ibm.com>
2016-08-11 17:08:23 +0800

    Added java deployable for Visual Recognition and Tone Analyzer

--- ImageProcessing/VisualRecognition/src/main/java/Snippet.java
+++ ImageProcessing/VisualRecognition/src/main/java/Snippet.java
@@ -1,23 +1,69 @@
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import javax.servlet.annotation.WebServlet;
+
 import org.apache.commons.io.FileUtils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
-import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualRecognitionOptions;
-public class VisualRecognitionExample {
-  public static void main(String[] args) throws IOException {
-    VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_19);
-    service.setApiKey("");    
-    File obama;    
-    URL url = new URL("https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/44_barack_obama%5B1%5D.jpg");
-    obama = new File("test.jpg");     
-    FileUtils.copyURLToFile(url, obama);     
-    VisualRecognitionOptions voptoins = new VisualRecognitionOptions.Builder().images(obama).build();
-	service.detectFaces(voptoins);
-	DetectedFaces result = service.detectFaces(voptoins).execute();
-    System.out.println(result.toString());
+
+@WebServlet("/")
+public class Snippet extends SuperGlue {
+
+	public class Parameters {
+		public String apiKey = "";
+		public String url = "https://www.whitehouse.gov/sites/whitehouse.gov/files/images/first-family/44_barack_obama%5B1%5D.jpg";
+	}
+	
+	@Override
+	public Object process(Object myBean) {	
+		 VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_19);
+	    service.setApiKey(((Parameters) myBean).apiKey);    
+	    File obama;    
+	    URL url = null;
+		try {
+			url = new URL(((Parameters) myBean).url);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    obama = new File("test.jpg");     
+	    try {
+			FileUtils.copyURLToFile(url, obama);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}     
+	    VisualRecognitionOptions voptoins = new VisualRecognitionOptions.Builder().images(obama).build();
+		service.detectFaces(voptoins);
+		DetectedFaces result = service.detectFaces(voptoins).execute();
+		
+		return result;
+	}
+	
+	@Override
+	protected Object getParameters() {
+		return new Parameters();
+	}
+	
+	public static void main(String[] args) {
+		Snippet myclass = new Snippet();
+		Parameters parameters = myclass.new Parameters();
+		//****** Process method contains the key logic ******
+		Object processResult = myclass.process(((Parameters) parameters));
+		
+		JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(processResult.toString()).getAsJsonObject();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		System.out.println(gson.toJson(json));
     }
+
 }
--- TextProcessing/ToneAnalyzer/src/main/java/Snippet.java
+++ TextProcessing/ToneAnalyzer/src/main/java/Snippet.java
@@ -1,25 +1,56 @@
+import javax.servlet.annotation.WebServlet;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.ToneAnalyzer;
 import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 
-public class Snippet {
-  public static void main(String[] args) {
-    ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
-    service.setUsernameAndPassword("",""); // SET YOUR USERNAME AND PASSWORD
-    String text =
-      "I know the times are difficult! Our sales have been "
-          + "disappointing for the past three quarters for our data analytics "
-          + "product suite. We have a competitive data analytics product "
-          + "suite in the industry. But we need to do our job selling it! "
-          + "We need to acknowledge and fix our sales challenges. "
-          + "We can’t blame the economy for our lack of execution! "
-          + "We are missing critical sales opportunities. "
-          + "Our product is in no way inferior to the competitor products. "
-          + "Our clients are hungry for analytical tools to improve their "
-          + "business outcomes. Economy has nothing to do with it.";
 
-    // Call the service and get the tone
-    ToneAnalysis tone = service.getTone(text, null).execute();
-    System.out.println(tone);
+@WebServlet("/")
+public class Snippet extends SuperGlue {
+	  
+	public class Parameters {
+		public String userName = "";
+		public String password = "";
+		public String text = "I know the times are difficult! Our sales have been "
+		          + "disappointing for the past three quarters for our data analytics "
+		          + "product suite. We have a competitive data analytics product "
+		          + "suite in the industry. But we need to do our job selling it! "
+		          + "We need to acknowledge and fix our sales challenges. "
+		          + "We can’t blame the economy for our lack of execution! "
+		          + "We are missing critical sales opportunities. "
+		          + "Our product is in no way inferior to the competitor products. "
+		          + "Our clients are hungry for analytical tools to improve their "
+		          + "business outcomes. Economy has nothing to do with it.";
+	}
+	
+	@Override
+	public Object process(Object myBean) {		
+		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
+	    service.setUsernameAndPassword(((Parameters) myBean).userName, ((Parameters) myBean).password); // SET YOUR USERNAME AND PASSWORD
+	   
+	    // Call the service and get the tone
+	    ToneAnalysis tone = service.getTone(((Parameters) myBean).text, null).execute();
+		
+		return tone.toString();
+	}
+	
+	@Override
+	protected Object getParameters() {
+		return new Parameters();
+	}
 
-  }
+	public static void main(String[] args) {
+		Snippet myclass = new Snippet();
+		Parameters parameters = myclass.new Parameters();
+		//****** Process method contains the key logic ******
+		Object processResult = myclass.process(((Parameters) parameters));
+		
+		JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(processResult.toString()).getAsJsonObject();
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		System.out.println(gson.toJson(json));
+	}
 }
